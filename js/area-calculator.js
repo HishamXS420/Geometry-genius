6ef01fc8abfa510b356f57c4cc6148e8b044cbc1
Hisham Ibn Mahbub <hishamxs420@gmail.com>
2024-03-31 03:21:53 +0600

    second commit

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -9,8 +9,7 @@ function calculateTriangleArea() {
   console.log(Area);
 
   // Show Triangle Area
-  const areaSpan = document.getElementById("triangle-area");
-  areaSpan.innerText = Area;
+  setElementInnerText("triangle-area", Area);
 }
 
 function calculateRectangleArea() {
@@ -24,6 +23,44 @@ function calculateRectangleArea() {
   // calculate area
   const Area = Width * Length;
 
-  const areaSpan = document.getElementById('rectangle-area');
-  areaSpan.innerText = Area;
+  setElementInnerText("rectangle-area", Area);
+}
+
+function calculateParallelogramArea() {
+  const Base = getInputValue("parallelogram-base");
+  const Height = getInputValue("parallelogram-height");
+
+  //    console.log(Base);
+  //     console.log(Height);
+
+  const Area = Base * Height;
+
+  setElementInnerText("parallelogram-area", Area);
+}
+
+function calculateEllipseArea() {
+  const majorRadius = getInputValue("ellipse-major-radius");
+  const minorRadius = getInputValue("ellipse-minor-radius");
+
+  const Area = 3.1416 * majorRadius * minorRadius;
+
+  setElementInnerText('ellipse-area', Area)
+}
+
+function calculateRhombusArea(){
+    const firstDiagonal = getInputValue("rhombus-first-diagonal");
+  const secondDiagonal = getInputValue("rhombus-second-diagonal");
+
+  const Area = 0.5 * firstDiagonal * secondDiagonal;
+  setElementInnerText('rhombus-area', Area);
+
+}
+
+function calculatePentagonArea(){
+    const perimeter = getInputValue("pentagon-perimeter");
+    const apothem = getInputValue("pentagon-apothem");
+
+    const Area = 0.5 * perimeter * apothem;
+  setElementInnerText('pentagon-area', Area);
+
 }
--- js/utilities.js
+++ js/utilities.js
@@ -1,8 +1,17 @@
-function getInputValue(elementId){
-    // get triangle base value
-    const Field = document.getElementById(elementId);
-    const FieldText = Field.value;
-    const FieldValue = parseFloat(FieldText)
-    
-    return FieldValue;
+// Reusable Input Value getter
+
+function getInputValue(elementId) {
+  // get triangle base value
+  const Field = document.getElementById(elementId);
+  const FieldText = Field.value;
+  const FieldValue = parseFloat(FieldText);
+
+  return FieldValue;
 }
+
+// Reusable set span, p , div, etc text
+function setElementInnerText(elementId, Area) {
+  const element = document.getElementById(elementId);
+  element.innerText = Area;
+}
+
